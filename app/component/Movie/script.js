4d48a6dce8160fa48da7a46918517ca47fb5a6dc
Meas Gary <meas5@mmi-dev.unilim.fr>
2026-04-23 16:43:36 +0200

    update credentials

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -2,13 +2,21 @@ let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
 let Movie = {};
+const IMAGE_FOLDER = "../server/images/";
 
 Movie.format = function (movie) {
   let html = template;
+  let imagePath = movie.image ? `${IMAGE_FOLDER}${movie.image}` : "";
   html = html.replaceAll("{{name}}", movie.name);
-  html = html.replaceAll("{{image}}", movie.image);
-  html = html.replaceAll("{{year}}", movie.year || "");
+  html = html.replaceAll("{{image}}", imagePath);
   return html;
+};
+
+Movie.formatMany = function (movies) {
+  if (!movies || movies.length === 0) {
+    return '<p>Aucun film disponible pour le moment.</p>';
+  }
+  return movies.map((movie) => Movie.format(movie)).join("");
 };
 
 export { Movie };
